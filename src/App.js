85a84a2425cf7454f72152866cec4c1a72c8f669
ak220193 <mail2meak22frcrio@gmail.com>
2024-02-27 20:03:58 +0530

    country Search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import './App.css';
+import Country from './Components/Countrysearch/Country.jsx';
+
 
 function App() {
+
   return (
     <div className="App">
-    
+    <Country/>
     </div>
   );
 }
